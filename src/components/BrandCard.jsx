59b99c8d57f90b24ff5d092fc22648fb11b652e0
omergafar05 <155408869+omergafar05@users.noreply.github.com>
2024-10-21 23:57:42 -0400

    Update Fields and Image Source
    Update field names and image source for future implementation to use team logos.

--- src/components/BrandCard.jsx
+++ src/components/BrandCard.jsx
@@ -4,3 +4,31 @@ import useStockCalls from "../service/useStockCalls";
 const TeamCard = ({ setData, team, handleOpen }) => {
   const { deleteStocks } = useStockCalls();
   const { teamName, logo, _id } = team; // Renamed fields
+  return (
+    <div className="block rounded-lg p-4 shadow-sm shadow-indigo-100 border-2 border-[#ABFB60]">
+      <img
+        alt="Team Logo"
+        src={logo} // Changed to logo
+        className="h-56 w-full rounded-md object-contain"
+      />
+      <div className="mt-2">
+        <dl className="flex justify-between">
+          <div>
+            <dt className="sr-only">Team Name</dt>
+            <dd className="text-md text-gray-500 font-bold">{teamName}</dd> {/* Changed to teamName */}
+          </div>
+        </dl>
+        <div className="flex justify-end">
+          <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
+            <button
+              className="inline-block border-e p-3 text-gray-900 bg-[#ABFB60] hover:bg-gray-200 focus:relative"
+              title="Edit Team"
+              onClick={() => { handleOpen(); setData(team); }} // Updated to team
+            >
+              {editIcon}
+            </button>
+            <button
+              className="inline-block p-3 text-gray-900 bg-[#ABFB60] hover:bg-gray-200 focus:relative"
+              title="Delete Team"
+              onClick={() => deleteStocks("teams", _id)} // Updated to teams
+            >
